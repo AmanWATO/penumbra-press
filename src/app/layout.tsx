7250bcb98efa30c478f43f537845eb648367e8b9
AmanWATO <aman@freecustomer.in>
2025-03-31 21:11:08 +0530

    Google Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/context/ThemeProvider";
 import { ConfigProvider } from "@/context/ConfigProvider";
 import { GlobalHeader } from "@/components/global-header";
 import { GlobalFooter } from "@/components/global-footer";
+import Script from "next/script";
 
 const playfair = Playfair_Display({
   variable: "--font-playfair",
@@ -22,6 +23,8 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
 });
 
+const GTM_ID = "GTM-K6S8HL33";
+
 export const metadata: Metadata = {
   title: "Penumbra Penned",
   description:
@@ -35,9 +38,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${playfair.variable} ${spaceGrotesk.variable} ${poppins.variable} antialiased`}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+
         <ThemeProvider>
           <ConfigProvider>
             <GlobalHeader />
